b1402716535c5e9b6bc87be69e4e90bf222bef40
masteryada378 <masteryada378@gmail.com>
2026-06-18 12:22:45 +0300

    upd

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -7,15 +7,12 @@ export function groupByKey<T, K extends keyof T>(
   key: K,
 ): GroupsMap<T> {
   return items.reduce((result, item) => {
-    // Отримуємо значення ключа, перетворюючи його на рядок
     const groupValue = String(item[key]);
 
-    // Якщо такої групи ще немає, створюємо порожній масив
     if (!result[groupValue]) {
       result[groupValue] = [];
     }
 
-    // Додаємо елемент до відповідної групи
     result[groupValue].push(item);
 
     return result;
