3adf6c3603d00c3daa307982c4cac1e6314f7235
masteryada378 <masteryada378@gmail.com>
2026-06-18 12:21:43 +0300

    groupByKey

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,22 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T, K extends keyof T>(
+  items: T[],
+  key: K,
+): GroupsMap<T> {
+  return items.reduce((result, item) => {
+    // Отримуємо значення ключа, перетворюючи його на рядок
+    const groupValue = String(item[key]);
+
+    // Якщо такої групи ще немає, створюємо порожній масив
+    if (!result[groupValue]) {
+      result[groupValue] = [];
+    }
+
+    // Додаємо елемент до відповідної групи
+    result[groupValue].push(item);
+
+    return result;
+  }, {} as GroupsMap<T>);
 }
